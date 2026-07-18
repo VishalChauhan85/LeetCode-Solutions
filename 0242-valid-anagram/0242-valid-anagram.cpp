e8e212c00a088aa31df18adcf2a2ad325786db94
Vishal Chauhan <chauhanvishal53381@gmail.com>
2026-07-19 01:41:36 +0530

    0242-valid-anagram

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> v1(26,0);
+        vector<int> v2(26,0);
+
+        for(char c:s){
+            v1[c-'a']++;
+        }
+        for(char c:t){
+            v2[c-'a']++;
+        }
+
+        return v1==v2;
+    }
+};
