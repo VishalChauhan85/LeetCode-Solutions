13c04201bfb67c1824972f2a5fbccb17a5ae6036
Vishal Chauhan <chauhanvishal53381@gmail.com>
2026-07-19 01:31:39 +0530

    0066-plus-one

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n=digits.size();
-        vector<int> v;
-        int carry=1;
         for(int i=n-1;i>=0;i--){
-            if(carry==1 && digits[i]==9){
-                v.push_back(0);
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
             }
-            else if(carry==1){
-                v.push_back(digits[i]+1);
-                carry=0;
-            }else if(carry==0){
-                v.push_back(digits[i]);
-            }
+            digits[i]=0;
         }
-        if(carry)v.push_back(1);
 
-        reverse(v.begin(),v.end());
-        return v;
+        digits[0]=1;
+        digits.push_back(0);
+
+        return digits;      
     }
 };
