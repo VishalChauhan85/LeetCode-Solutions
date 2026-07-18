a3c1b29eb21812f1310ce5d03439427bf26cb39c
Vishal Chauhan <chauhanvishal53381@gmail.com>
2026-07-19 01:50:48 +0530

    0268-missing-number

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(),nums.end());
+        int sum=(n*(n+1))/2;
+        sum-=accumulate(nums.begin(),nums.end(),0);
 
-        for(int i=0;i<n;i++){
-            if(nums[i]!=i)return i;
-        }
-        return n;
+        return sum;
     }
 };
