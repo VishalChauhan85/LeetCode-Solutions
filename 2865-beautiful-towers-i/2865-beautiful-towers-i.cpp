8bb94ab8f12431b9cd3e5100cdc4fb3f30845d6d
Vishal Chauhan <chauhanvishal53381@gmail.com>
2026-08-01 01:45:44 +0530

    2865-beautiful-towers-i

--- 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     long long maximumSumOfHeights(vector<int>& heights) {
         int n=heights.size();
-        vector<long long> left(n,-1);
-        vector<long long> right(n,n);
+        vector<long long> left(n);
+        vector<long long> right(n);
         stack<int> s;
 
-        long long sum=0;
         for(int i=0;i<n;i++){
             while(!s.empty() && heights[i]<heights[s.top()]){
                 s.pop();
             }
             if(!s.empty()){
-                left[i]=1LL*left[s.top()]+1LL*heights[i]*(i-s.top());
+                left[i]=left[s.top()]+1LL*heights[i]*(i-s.top());
             }else{
                 left[i]=1LL*heights[i]*(i+1);
             }
@@ -20,7 +19,6 @@ public:
             s.push(i);
         }
 
-        sum=0;
         while(!s.empty())s.pop();
 
         for(int i=n-1;i>=0;i--){
@@ -28,14 +26,14 @@ public:
                 s.pop();
             }
             if(!s.empty()){
-                right[i]=1LL*right[s.top()]+1LL*heights[i]*(s.top()-i);
+                right[i]=right[s.top()]+1LL*heights[i]*(s.top()-i);
             }else{
                 right[i]=1LL*heights[i]*(n-i);
             }
             s.push(i);
         }
 
-        sum=0;
+        long long sum=0;
         for(int i=0;i<n;i++){
             sum=max(sum,1LL*left[i]+right[i]-heights[i]);
         }
