27b10f1c8b7f712e463a209499322df5b85be933
Vishal Chauhan <chauhanvishal53381@gmail.com>
2026-08-01 17:23:16 +0530

    0643-maximum-average-subarray-i

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
         int n=nums.size();
-        int p1=0;
-        int p2=k;
-
         double num=0;
         for(int i=0;i<k;i++)num+=nums[i];
         double ans=num;
         
-        for(p2;p2<n;p2++,p1++){
-            num=num+nums[p2]-nums[p1];
+        for(int p2=k;p2<n;p2++){
+            num=num+nums[p2]-nums[p2-k];
             ans=max(ans,num);
         }
 
