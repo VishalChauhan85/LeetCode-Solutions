40e4afd1476649bd08fbe29fe886a49af2cbd2f3
Vishal Chauhan <chauhanvishal53381@gmail.com>
2026-08-23 02:00:32 +0530

    0022-generate-parentheses

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void solve(vector<string>& ans,string s,int n,int m){
+        if(m==0 && n==0){
+            ans.push_back(s);
+            return;
+        }
+
+        if(m>0)solve(ans,s+')',n,m-1);
+        if(n>0)solve(ans,s+'(',n-1,m+1);
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        solve(ans,"",n,0);
+        return ans;        
+    }
+};
