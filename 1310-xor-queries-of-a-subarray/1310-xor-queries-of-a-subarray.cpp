0aba471e6dc3155ac396ba8da0913a21bb315fd2
Vishal Chauhan <chauhanvishal53381@gmail.com>
2026-08-28 23:07:58 +0530

    1310-xor-queries-of-a-subarray

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -2,18 +2,11 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         int n=arr.size();
-        vector<int> v(n+1);
-        v[0]=0;
-        for(int i=1;i<=n;i++){
-            v[i]=v[i-1]^arr[i-1];
-        }
-
-        vector<int> ans(queries.size(),0);
-        int m=queries.size();
-        for(int i=0;i<m;i++){
-            ans[i]=v[queries[i][0]]^v[queries[i][1]+1];
-        }
-
+        vector<int> v(n+1,0);
+        vector<int> ans;
+        
+        for(int i=1;i<=n;i++)v[i]=v[i-1]^arr[i-1];
+        for(auto a:queries)ans.push_back(v[a[0]]^v[a[1]+1]);
         return ans;
     }
 };
